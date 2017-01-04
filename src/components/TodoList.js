880be174df67b8447a9d7d2343c583a0fa6c2599
Beth Qiang <bethany.qiang@gmail.com>
2017-01-03 20:41:39 -0600

    Add test for active filter & test passing
    Add filterItems function to TodoList. TodoList no longer a completely presentational component.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,17 +2,26 @@ import React from 'react';
 
 import TodoItem from './TodoItem';
 
-const TodoList = props => {
-  return (
-    <section className="main">
-      <ul className="todo-list">
-        {props.todos.map(item => (
-          <TodoItem key={item.get('text')}
-            text={item.get('text')} />
-        ))}
-      </ul>
-    </section>
-  );
-};
+class TodoList extends React.Component {
+  filterItems() {
+    if (this.props.todos) {
+      return this.props.todos.filter(item => (
+        item.get('status') === this.props.filter
+      ));
+    }
+  }
+  render() {
+    return (
+      <section className="main">
+        <ul className="todo-list">
+          {this.filterItems().map(item => (
+            <TodoItem key={item.get('text')}
+              text={item.get('text')} />
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
 
 export default TodoList;
---
+++ test/TodoList.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import TodoList from '../src/components/TodoList';
+import { expect } from 'chai';
+import { List, Map } from 'immutable';
+
+const { renderIntoDocument, scryRenderedDOMComponentsWithTag } = TestUtils;
+
+describe('TodoList', () => {
+  it('renders a list of active items when the filter is set to active', () => {
+    const todos = List.of(
+      Map({ id: 1, text: 'React', status: 'active' }),
+      Map({ id: 2, text: 'Redux', status: 'active' }),
+      Map({ id: 3, text: 'Immutable', status: 'completed' })
+    );
+    const filter = 'active';
+    const component = renderIntoDocument(
+      <TodoList filter={filter} todos={todos} />
+    );
+    const items = scryRenderedDOMComponentsWithTag(component, 'li');
+
+    expect(items.length).to.equal(2);
+    expect(items[0].textContent).to.contain('React');
+    expect(items[1].textContent).to.contain('Redux');
+  });
+});
